e2f57caeabb701a51aa1bea1b41f88bae60011b2
HalianRC <125613474+HalianRC@users.noreply.github.com>
2023-05-22 21:09:29 -0300

    quase-pronto

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,22 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+
+function classKenzie(){
+  const openModalBtn = document.querySelector("#openModal")
+  const closeModalBtn = document.querySelector("#closeModal")
+  const sendEmailBtn = document.querySelector("#listNextClass")
+  const myModal = document.querySelector("#myModal")
+
+  openModalBtn.addEventListener("click",function(){
+    myModal.showModal()
+  })
+
+  closeModalBtn.addEventListener('click', () => {
+    myModal.close();
+  });
+
+  sendEmailBtn.addEventListener('click', () => {
+    myModal.close();
+  });
+
+}
+classKenzie()
